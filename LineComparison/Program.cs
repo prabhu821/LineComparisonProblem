5931070b005ceb0e0aa8284014584dd32e71ea35
rishiraj007 <rishiraj@mounttalent.com>
2022-12-02 17:22:33 +0530

    [Prabhu]: Calculate Length

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace LineComparison
+﻿using static LineComparison.LineCompare;
+
+namespace LineComparison
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome To Line Comparision Problem Statement");
+            LineCompare lineCompare = new LineCompare(2, 4, 6, 8);
+            lineCompare.CalculateLength();
         }
     }
 }
